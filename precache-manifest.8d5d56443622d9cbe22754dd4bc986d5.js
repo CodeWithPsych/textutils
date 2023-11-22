f094d7eda54ec768ec0f5d39695558afb77a762f
PsychPrograming <127660918+PsychPrograming@users.noreply.github.com>
2023-11-23 02:18:50 +0500

    Updates

---
+++ precache-manifest.8d5d56443622d9cbe22754dd4bc986d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99377e19c66dd3cab09677d5121d2597",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "5740edde918fa47d2738",
+    "url": "/textutils/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9975cebdafd6710787bf",
+    "url": "/textutils/static/js/2.2ad8f820.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.2ad8f820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5770b6dbafba458b69",
+    "url": "/textutils/static/js/3.9d25297e.chunk.js"
+  },
+  {
+    "revision": "5740edde918fa47d2738",
+    "url": "/textutils/static/js/main.98e5a76b.chunk.js"
+  },
+  {
+    "revision": "be3a5429e865666ce8e7",
+    "url": "/textutils/static/js/runtime-main.28099f2f.js"
+  }
+]);
